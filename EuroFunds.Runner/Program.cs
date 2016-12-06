e36435834d0218b009998df1941e82d63ff27851
Imiolak <maciej.imiolek@gmail.com>
2016-12-06 21:39:37 +0100

    Simple stats

--- EuroFunds.Runner/Program.cs
+++ EuroFunds.Runner/Program.cs
@@ -1,12 +1,16 @@
-﻿namespace EuroFunds.Runner
+﻿using EuroFunds.Statistics;
+
+namespace EuroFunds.Runner
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var loader = new DataLoader.DataLoader();
+            //var loader = new DataLoader.DataLoader();
+            //loader.Load();
 
-            loader.Load();
+            var statisticsGenerator = new StatisticsGenerator();
+            //do sth with stats here
         }
     }
 }
---
+++ EuroFunds.Statistics/StatisticsGenerator.cs
@@ -0,0 +1,68 @@
+﻿using EuroFunds.Database.DAO;
+using EuroFunds.Database.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EuroFunds.Statistics
+{
+    public class StatisticsGenerator
+    {
+        public IDictionary<string, decimal> SumOfTotalProjectValuesForEachLocation()
+        {
+            var map = new Dictionary<string, decimal>();
+
+            using (var context = new EuroFundsContext())
+            {
+                var projectLocations = context.ProjectLocations.ToList();
+
+                foreach (var projectLocation in projectLocations.Where(pl => pl.Name != ProjectLocation.WholeCountry.Name))
+                {
+                    map[projectLocation.Name] = projectLocation.Projects.Sum(project => project.TotalProjectValue);
+                }
+
+                foreach (var projectValue in projectLocations
+                    .Single(pl => pl.Name == ProjectLocation.WholeCountry.Name)
+                    .Projects
+                    .Select(project => project.TotalProjectValue))
+                {
+                    foreach (var key in map.Keys.ToList())
+                    {
+                        map[key] += projectValue;
+                    }
+                }
+            }
+
+            return map;
+        }
+
+        public IDictionary<string, int> NumberOfProjectsForEachLocation()
+        {
+            using (var context = new EuroFundsContext())
+            {
+                var map = context.ProjectLocations
+                    .Where(pl => pl.Name != ProjectLocation.WholeCountry.Name)
+                    .ToDictionary(pl => pl.Name, pl => pl.Projects.Count);
+
+                var wholeCountryProjects = context.ProjectLocations
+                    .Single(pl => pl.Name != ProjectLocation.WholeCountry.Name)
+                    .Projects
+                    .Count;
+
+                foreach (var key in map.Keys.ToList())
+                {
+                    map[key] += wholeCountryProjects;
+                }
+
+                return map;
+            }
+        }
+
+        public IDictionary<string, decimal> AverageTotalProjectValueForEachLocation()
+        {
+            var totalValues = SumOfTotalProjectValuesForEachLocation();
+            var noProjects = NumberOfProjectsForEachLocation();
+
+            return totalValues.ToDictionary(kv => kv.Key, kv => kv.Value / noProjects[kv.Key]);
+        }
+    }
+}
