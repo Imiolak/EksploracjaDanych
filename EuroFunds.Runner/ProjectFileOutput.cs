1d0b039262cf79fd9f2144dd14c41598d136763f
Imiolak <maciej.imiolek@gmail.com>
2016-12-21 15:25:02 +0100

    Split multiple loations into multiple lines

--- EuroFunds.Runner/ProjectFileOutput.cs
+++ EuroFunds.Runner/ProjectFileOutput.cs
@@ -21,31 +21,30 @@ namespace EuroFunds.Runner
                     foreach (var project in projects)
                     {
                         var sb = new StringBuilder();
-
                         sb.Append(project.TotalProjectValue);
                         sb.Append($", {project.ProjectStartDate.Year}");
                         sb.Append($", {project.GetProjectLength().TotalDays}");
-                        sb.Append($", {project.AreaOfEconomicActivity.OrderNo}");
-                        sb.Append(", ");
+                        sb.Append($", {project.AreaOfEconomicActivity.OrderNo}, ");
+
+                        var baseString = sb.ToString();
 
                         foreach (var projectLocation in project.ProjectLocations)
                         {
                             if (projectLocation.Name == ProjectLocation.WholeCountry.Name)
                             {
-                                foreach (
-                                    var location in
+                                foreach (var location in
                                         context.ProjectLocations.ToList().Where(pl => ProjectLocation.IsInPoland(pl.Name)))
                                 {
-                                    sb.Append($"{location.Name};");
+                                    file.WriteLine($"{baseString}{location.Name}");
                                 }
                             }
                             else
                             {
-                                sb.Append($"{projectLocation.Name};");
+                                file.WriteLine($"{baseString}{projectLocation.Name}");
                             }
                         }
 
-                        file.WriteLine(sb.ToString().TrimEnd(';'));
+                        
                     }
                 }
 
